ef37e919d0ebabbe8011d42fa6c05f3d3664ff0c
Patrick Beraguas Pereira <patrick.pereira@encora.com>
2023-10-04 11:28:32 -0300

    python studies updated 04.10

--- examples/examples.py
+++ examples/examples.py
@@ -1,4 +1,4 @@
-
+###############################################################################################################
 
 # How to manipulate outputs printing strings and numbers
 
@@ -7,6 +7,8 @@ def print_example():
     print(2, 'this is the second line')
     print(3, 'this is the third line')
 
+
+###############################################################################################################
 
 # Inputs
 # Example which user must interact and type values in the terminal,
@@ -26,6 +28,8 @@ def inputs_example():
     print(text)
 
 
+###############################################################################################################
+
 # Formatted strings in Python
 # used to reduce complexity while writing texts, see example below using a pure string vs formatted string, check that results are equals.
 
@@ -39,6 +43,8 @@ def formatted_strings(name: str, surname: str, city: str):
     print(formattedText)
 
 
+###############################################################################################################
+
 # String methods in Python
 # used to transform a string text into lowercase, uppercase, find a specific position, etc... I can just add . after var
 
@@ -51,6 +57,8 @@ def string_methods_example():
     print(methodText.split())
     print(methodText.find('WEBSITES'))
 
+
+###############################################################################################################
 
 # Math operations in Python
 
@@ -66,12 +74,14 @@ def math_operations_example():
     print(mathTimes)
 
 
+###############################################################################################################
 
 # Comparisson Operators in Python
 
 def comparisson_true_operators_example():
 
     # outputs below must be True
+
     ops1 = 10 == 10
     ops2 = 'blue' != 'purple'
     ops3 = 10 > 6
@@ -80,10 +90,12 @@ def comparisson_true_operators_example():
     ops6 = 1 <= 2
 
     true_ops_str = '\n'.join(str(op)
-        for op in [ops1, ops2, ops3, ops4, ops5, ops6])
+                             for op in [ops1, ops2, ops3, ops4, ops5, ops6])
 
     print(true_ops_str)
 
+
+###############################################################################################################
 
 def comparisson_false_operators_example():
 
@@ -97,7 +109,76 @@ def comparisson_false_operators_example():
     ops12 = 9 <= 3
 
     false_ops_str = '\n'.join(str(op)
-        for op in [ops7, ops8, ops9, ops10, ops11, ops12])
+                              for op in [ops7, ops8, ops9, ops10, ops11, ops12])
 
     print(false_ops_str)
 
+
+###############################################################################################################
+
+
+# Conditional Statements
+
+def conditional_example(speed: int):
+
+    if speed > 100:
+        print('Velocidade acima do permitido')
+        print('reduza a velocidade')
+    elif speed < 40:
+        print('Velocidade minima atingida')
+        print('Aumente a velocidade')
+    else:
+        print('Velocidade apropriada para rodovia')
+
+
+###############################################################################################################
+
+
+# Logical Operators
+
+
+def logical_operators_example(salary: int, clean_name: bool, has_property: bool):
+
+    if salary > 5000 and clean_name == True and has_property == True:
+        print('Financiamento aprovado')
+    elif salary > 3000 and clean_name == True and has_property == True:
+        print('Financiamento pendente, avaliacao salarial')
+    else:
+        print('Financiamento negado')
+
+
+###############################################################################################################
+
+
+# Multiple Comparison Operators
+
+def multiple_logical_operators(value: int):
+
+    if value >= 1 and value < 10:
+        print('Produto dentro do range aceito')
+    else:
+        print('Produto fora do range e nao aceito')
+
+
+###############################################################################################################
+
+
+# For Loop (Numbers)
+
+
+def for_loop_numbers():
+
+    for num in range(1, 11):
+        print(num)
+
+
+###############################################################################################################
+
+
+# For Loop (Strings)
+
+
+def for_loop_strings(word: str):
+
+    for string in word:
+        print(f'{string} esta dentro da palavra {word}')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 from examples.examples import (
     print_example, 
     inputs_example, 
@@ -6,14 +7,22 @@ from examples.examples import (
     string_methods_example, 
     math_operations_example, 
     comparisson_true_operators_example,
-    comparisson_false_operators_example
+    comparisson_false_operators_example,
+    conditional_example,
+    logical_operators_example,
+    multiple_logical_operators,
+    for_loop_numbers,
+    for_loop_strings,
+    
 )
 
-from functions.greeting import (
-    dynamic_greeting
-)
+
+
 
 def Principal():
+    
+    #Examples 
+
     #print_example()
     #inputs_example()
     #formatted_strings(name='Patrick', surname='Pereira', city='SP')
@@ -21,6 +30,12 @@ def Principal():
     #math_operations_example()
     #comparisson_true_operators_example()
     #comparisson_false_operators_example()
-    dynamic_greeting(name='Patrick')
+    #conditional_example(speed= 100)
+    #logical_operators_example(salary= 4000, clean_name= True, has_property= True,)
+    #multiple_logical_operators(value=12)
+    #for_loop_numbers()
+    
+    for_loop_strings(word="Apple")
+    
 
 Principal()
